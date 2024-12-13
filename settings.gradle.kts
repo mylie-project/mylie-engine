d95d16228fc2a6608b3c188d823ea459795524b8
zzuegg <zzuegg@gmail.com>
2024-12-13 11:05:21 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+rootProject.name = "mylie-engine"
+
+dependencyResolutionManagement{
+    versionCatalogs{
+        create("libs"){
+            version("lombok","1.18.34")
+            version("logback","1.5.12")
+            version("slf4j","2.0.16")
+            version("joml","1.10.7")
+            version("lwjgl3","3.3.4")
+            version("imgui","1.87.6")
+
+            library("lombok","org.projectlombok","lombok").versionRef("lombok")
+            library("logging.api","org.slf4j","slf4j-api").versionRef("slf4j")
+            library("logging.runtime","ch.qos.logback","logback-classic").versionRef("logback")
+            library("joml","org.joml","joml").versionRef("joml")
+            library("lwjgl3.imgui","io.github.spair","imgui-java-app").versionRef("imgui")
+            library("lwjgl3.core","org.lwjgl","lwjgl").versionRef("lwjgl3")
+            library("lwjgl3.glfw","org.lwjgl","lwjgl-glfw").versionRef("lwjgl3")
+            library("lwjgl3.opengl","org.lwjgl","lwjgl-opengl").versionRef("lwjgl3")
+            library("lwjgl3.stb","org.lwjgl","lwjgl-stb").versionRef("lwjgl3")
+            library("lwjgl3.core.natives","org.lwjgl","lwjgl").versionRef("lwjgl3")
+            library("lwjgl3.glfw.natives","org.lwjgl","lwjgl-glfw").versionRef("lwjgl3")
+            library("lwjgl3.opengl.natives","org.lwjgl","lwjgl-opengl").versionRef("lwjgl3")
+            library("lwjgl3.stb.natives","org.lwjgl","lwjgl-stb").versionRef("lwjgl3")
+
+        }
+    }
+}
