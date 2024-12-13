91f5998e43c6084bc321b96af16d2efd2085f4fa
zzuegg <zzuegg@gmail.com>
2024-12-13 13:02:03 +0100

    [Add] [AutoIncrementedTest] Tests for reference value and version synchronization [Add] [AutoIncrementedTest] Test for detecting updates when parent value changes [Add] [AutoIncrementedTest] Test for no change detection when parent value remains [Add] [AutoIncrementedTest] Test for synchronizing after reference rechecks parent [Add] [AutoIncrementedTest] Test for reference updates when parent value is null

--- engine/core/src/test/java/mylie/util/versioned/AutoIncrementedTest.java
+++ engine/core/src/test/java/mylie/util/versioned/AutoIncrementedTest.java
@@ -68,4 +68,63 @@ public class AutoIncrementedTest {
 
         assertEquals(1, versionAfterUpdate);
     }
+
+
+    @Test
+    public void whenReferenceIsCreated_thenValueAndVersionMatchParent() {
+        AutoIncremented<String> autoIncrement = new AutoIncremented<>("initial");
+        Versioned.Reference<String> reference = autoIncrement.reference();
+
+        assertEquals("initial", reference.value());
+        assertEquals(autoIncrement.version(), reference.version());
+    }
+
+    @Test
+    public void whenParentValueChanges_thenReferenceDetectsChangeAndUpdates() {
+        AutoIncremented<String> autoIncrement = new AutoIncremented<>("initial");
+        Versioned.Reference<String> reference = autoIncrement.reference();
+
+        autoIncrement.value("updated");
+
+        assertTrue(reference.changed());
+        assertEquals("updated", reference.value());
+        assertEquals(autoIncrement.version(), reference.version());
+    }
+
+    @Test
+    public void whenParentValueDoesNotChange_thenReferenceDetectsNoChange() {
+        AutoIncremented<String> autoIncrement = new AutoIncremented<>("initial");
+        Versioned.Reference<String> reference = autoIncrement.reference();
+
+        autoIncrement.value("initial");
+
+        assertFalse(reference.changed());
+        assertEquals("initial", reference.value());
+        assertEquals(autoIncrement.version(), reference.version());
+    }
+
+    @Test
+    public void whenReferenceRechecksParent_thenVersionAndValueSynchronize() {
+        AutoIncremented<String> autoIncrement = new AutoIncremented<>("initial");
+        Versioned.Reference<String> reference = autoIncrement.reference();
+
+        autoIncrement.value("updated");
+        reference.value();
+
+        assertFalse(reference.changed());
+        assertEquals("updated", reference.value());
+        assertEquals(autoIncrement.version(), reference.version());
+    }
+
+    @Test
+    public void whenParentValueIsNull_thenReferenceUpdatesToNull() {
+        AutoIncremented<String> autoIncrement = new AutoIncremented<>("initial");
+        Versioned.Reference<String> reference = autoIncrement.reference();
+
+        autoIncrement.value(null);
+
+        assertTrue(reference.changed());
+        assertNull(reference.value());
+        assertEquals(autoIncrement.version(), reference.version());
+    }
 }
